b0767babda1da15632e80a7c6cae0c4aa2e8fc34
timvest <73234307+timvest@users.noreply.github.com>
2024-10-08 14:18:56 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,12 +1,12 @@
 print("Enter your first number:")
-first_number = float(input())
+first_number = int(input())
 print("Enter your second number:")
-sec_number = float(input())
+sec_number = int(input())
 print(first_number,"x",sec_number,"=",int(first_number*sec_number))
 num = int(first_number*sec_number)
 if num <0:
     print("The result is a negative.")
 if num >0:
     print("The result is a positive.")
-if num == (0):
+else:
     print("The result is both a negative and positive.")
