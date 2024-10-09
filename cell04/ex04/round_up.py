fd7fb9946a41c20214a3c5bd8cc7522cd4b247fd
timvest <73234307+timvest@users.noreply.github.com>
2024-10-09 14:26:48 +0700

    Add files via upload

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,5 @@
+import math
+
+A = (input("Give me a number: "))
+B = float(A)#changes int to float ,which makes rounding it possible
+print(math.ceil(B)) #round() rounds the number up #print(round(B))
